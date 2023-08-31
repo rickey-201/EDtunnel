1a352e4ff6a9494469b174bf39848e3f4cdb0a20
rickey-201 <95132501+rickey-201@users.noreply.github.com>
2023-08-31 16:40:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,9 +5,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b4c5eeb2-c34b-4c06-a9ad-99214f185ac0';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = [];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
